b77b7d8345daa9d5932593036914f8b6779c8124
elise <USIS034221@ugb.edu.sv>
2023-05-19 01:47:55 -0600

    Se agrego el login y el registro

---
+++ app/src/main/java/com/example/usuplace/activites/LoginActivity.java
@@ -0,0 +1,82 @@
+package com.example.usuplace.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.usuplace.DBmanager;
+import com.example.usuplace.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewSignUp;
+
+    private DBmanager dbManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        dbManager = new DBmanager(this);
+
+        // Obtener referencias de los elementos de la interfaz
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewSignUp = findViewById(R.id.textViewSignUp);
+
+        // Configurar el evento de clic del botón de inicio de sesión
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+        // Configurar el evento de clic del texto de registro
+        textViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+
+    private void loginUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        // Validar los campos
+        if (email.isEmpty()) {
+            Toast.makeText(this, "Ingrese su correo electrónico", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            Toast.makeText(this, "Ingrese su contraseña", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Verificar las credenciales del usuario en la base de datos
+        if (dbManager.checkUserCredentials(email, password)) {
+            // Credenciales válidas, redirigir al usuario a la siguiente actividad
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // Credenciales inválidas, mostrar un mensaje de error
+            Toast.makeText(this, "Credenciales inválidas", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/usuplace/activites/RegisterActivity.java
@@ -0,0 +1,80 @@
+package com.example.usuplace.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.usuplace.R;
+import com.example.usuplace.DBmanager;
+import com.example.usuplace.User;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextEmail;
+    private EditText editTextPhone;
+    private EditText editTextMunicipality;
+    private EditText editTextDepartment;
+    private EditText editTextPassword;
+    private Button buttonRegister;
+
+    private DBmanager dbManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        dbManager = new DBmanager(this);
+
+        // Obtener referencias de los elementos de la interfaz
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextMunicipality = findViewById(R.id.editTextMunicipality);
+        editTextDepartment = findViewById(R.id.editTextDepartment);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        // Configurar el evento de clic del botón de registro
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        // Obtener los valores ingresados por el usuario
+        String name = editTextName.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String phone = editTextPhone.getText().toString().trim();
+        String municipality = editTextMunicipality.getText().toString().trim();
+        String department = editTextDepartment.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        // Validar los campos (puedes agregar tus propias validaciones aquí)
+
+        // Verificar si el usuario ya existe
+        if (dbManager.checkUserExists(email)) {
+            Toast.makeText(this, "El correo electrónico ya está registrado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Crear un objeto User con los datos ingresados
+        User user = new User(name, email, phone, municipality, department, password);
+
+        // Agregar el usuario a la base de datos
+        dbManager.addUser(user);
+
+        // Mostrar un mensaje de éxito
+        Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+
+        // Puedes agregar aquí el código para redirigir al usuario a otra actividad después del registro
+    }
+}
