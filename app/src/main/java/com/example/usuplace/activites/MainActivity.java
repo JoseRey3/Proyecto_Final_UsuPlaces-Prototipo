396ca786eac3ec4d6d397789aa9d2c595b3c80c1
ChristianMonte <cristian.alex24@gmail.com>
2023-05-19 11:13:56 -0600

    Cambios

--- app/src/main/java/com/example/usuplace/activites/MainActivity.java
+++ app/src/main/java/com/example/usuplace/activites/MainActivity.java
@@ -39,10 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
         //Place
         ArrayList<ItemDomain> ItemArraylist = new ArrayList<>();
-        ItemArraylist.add(new ItemDomain("Casa de campo1", "San José, San Salvador", "Es el mejor lugar para vacacionar", 12, "pic1"));
-        ItemArraylist.add(new ItemDomain("Casa de campo2", "San José, San Salvador", "Es el mejor lugar para vacacionar", 12, "pic2"));
-        ItemArraylist.add(new ItemDomain("Casa de campo3", "San José, San Salvador", "Es el mejor lugar para vacacionar", 12, "pic1"));
-        ItemArraylist.add(new ItemDomain("Casa de campo4", "San José, San Salvador", "Es el mejor lugar para vacacionar", 12, "pic2"));
+        ItemArraylist.add(new ItemDomain("Casa de campo1", "Usulután, Usulután", "Es el mejor lugar para vacacionar", 12, "pic1"));
+        ItemArraylist.add(new ItemDomain("Casa de campo2", "Santa María, Usulután", "Es el mejor lugar para vacacionar", 12, "pic2"));
+        ItemArraylist.add(new ItemDomain("Casa de campo3", "Tecapán, Usulután", "Es el mejor lugar para vacacionar", 12, "pic1"));
+        ItemArraylist.add(new ItemDomain("Casa de campo4", "Alegría, Usulután", "Es el mejor lugar para vacacionar", 12, "pic2"));
 
         recyclerViewPlace= findViewById(R.id.itemRV);
         recyclerViewPlace.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
